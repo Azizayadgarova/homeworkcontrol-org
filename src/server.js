6331485c1d7d52896c6785e10fef4c1e57c355b4
yadgarovtthjjiiiiiiiiiiiiiiiiiiii <mamuray819@gmail.com>
2025-12-22 20:44:19 +0500

    cone

--- src/server.js
+++ src/server.js
@@ -3,11 +3,15 @@ const express = require('express')
 const connectDB = require('./config/db')
 const cors = require('cors')
 
+// Routes
 const authRoutes = require('./routes/authRoutes')
 const groupRoutes = require('./routes/groupRoutes')
 const taskRoutes = require('./routes/taskRoutes')
 const submissionRoutes = require('./routes/submissionRoutes')
+
+// Swagger
 const { swaggerDocs } = require('./config/swagger')
+
 const app = express()
 connectDB()
 
@@ -19,9 +23,12 @@ app.use('/api/auth', authRoutes)
 app.use('/api/groups', groupRoutes)
 app.use('/api/tasks', taskRoutes)
 app.use('/api/submissions', submissionRoutes)
-swaggerDocs(app)
-// 404
+
+// Swagger
+swaggerDocs(app, process.env.PORT || 5000)
+
+// 404 handler
 app.use((req, res) => res.status(404).json({ message: 'Not Found' }))
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
